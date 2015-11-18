e45a918e8913cf6d2d3eecfac3756f935f38e212
yohanstra <jreinstra@gmail.com>
2015-11-17 22:24:58 -0800

    Fixed recommendation bug and added some optimizations

--- js/rec.js
+++ js/rec.js
@@ -36,9 +36,12 @@ function getMovieScore(movie, choices) {
     var genScore = listScore(movie, choices, "Genres");
     var popScore = movie["PopularityScore"];
     
-    var result = 100 * actScore + 200 * genScore + 40 * popScore;
-    //console.log(movie["Title"]);
-    //console.log(result);
+    var result = 100 * actScore + 200 * genScore;// + 40 * popScore;
+    if (movie["Title"] == "Ant-Man") {
+        console.log(movie["Title"]);
+        console.log(actScore + " " + genScore);// + " " + popScore);
+        console.log(result);
+    }
     return result;
 }
 
@@ -68,15 +71,20 @@ function itemScore(item, choices, key) {
                 }
             }
         }
+        var result;
         if(netOverlaps > 0) {
-            itemsCache[item] = 100.0 - (70.0 / netOverlaps);
+            result = 100.0 - (70.0 / netOverlaps);
         }
         else if(netOverlaps < 0) {
-            itemsCache[item] = (-70.0 / netOverlaps) - 100.0;
+            result = (-70.0 / netOverlaps) - 100.0;
         }
         else {
-            itemsCache[item] = 0;
+            result = 0;
         }
+        itemsCache[item] = result;
+        return result;
     }
-    return itemsCache[item];
+    else {
+        return itemsCache[item];
+    }
 }
